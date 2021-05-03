49565a71c7c32aacffc46fab88448056f046499e
LevDomasnih <lev.domasnix2013@gmail.com>
2021-05-03 19:50:48 +0500

    Initial commit

---
+++ src/main/java/com/example/graphqltest/mutation/VehicleMutation.java
@@ -0,0 +1,16 @@
+package com.example.graphqltest.mutation;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.example.graphqltest.entity.Vehicle;
+import com.example.graphqltest.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+    @Autowired
+    private VehicleService vehicleService;
+    public Vehicle createVehicle(final String type, final String modelCode, final String brandName, final String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
